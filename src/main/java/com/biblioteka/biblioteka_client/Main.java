7fe7a9330450e84bf6e106eec353340dadd91650
ilijaradojkovic <radojkovicika@gmail.com>
2022-04-05 14:37:26 +0300

    Client side projekat kreiran

---
+++ src/main/java/com/biblioteka/biblioteka_client/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.biblioteka.biblioteka_client;
+
+/**
+ *
+ * @author radoj
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
